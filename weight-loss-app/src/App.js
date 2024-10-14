9531f77d2200a927503e946528333cdcf628df43
Kai Bell <kaibell241@gmail.com>
2024-10-14 14:24:49 -0400

    Added meal plan generation

--- weight-loss-app/src/App.js
+++ weight-loss-app/src/App.js
@@ -4,18 +4,33 @@ import Header from './components/Header';
 import InputForm from './components/InputForm';
 import Results from './components/Results';
 import Footer from './components/Footer';
+import MealPlanForm from './components/MealPlanForm';
+import MealPlanResults from './components/MealPlanResults';
 
 function App() {
   const [results, setResults] = useState(null)
+  const [mealPlanResults, setMealPlanResults] = useState(null)
   
   return (
     <Router>
       <Header/>
         <div style={{ marginTop: `3em` }}>
           <Routes>
-            <Route path="/" element={<InputForm setResults={setResults} />} />
+            <Route path="/" element={
+              <>
+              <InputForm setResults={setResults} />
+              {results && <Results data={results}/>}
+              </>
+              }
+            />
+            <Route path="/meal-plan-generator" element={
+              <>
+              <MealPlanForm setMealPlanResults={setMealPlanResults}/>
+              {mealPlanResults && <MealPlanResults data={mealPlanResults}/>}
+              </>
+              }
+            />
           </Routes>
-          {results && <Results data={results}/>}
         </div>
       <Footer />
     </Router>
--- weight-loss-app/src/components/Header.js
+++ weight-loss-app/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
         <Menu.Item as={NavLink} to="/" header className="weight-loss-app">
         Weight Loss App
         </Menu.Item>
-        <Menu.Item as={NavLink} to="/meal-plan" className="meal-plan-item">
+        <Menu.Item as={NavLink} to="/meal-plan-generator" className="meal-plan-generator">
         Meal Plan Generator
         </Menu.Item>
     </Menu>
